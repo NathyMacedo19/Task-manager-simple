f82ddd5472fbd7a12d33f37645027fffe64cd0f7
Nathalia Macedo <nathy.fmacedo19@gmail.com>
2026-02-09 11:37:01 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// This file contains JavaScript code for the task manager application.
+
+document.getElementById("completeTask").addEventListener("click", () => {
+  alert("Task completed! Notification sent.");
+});
